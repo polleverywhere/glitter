d973b83d2238a46ea639d4505cb203a5d889ebd4
Petri Avikainen <petri.avikainen@gmail.com>
2015-02-12 14:05:15 +0200

    add support for minimum system version

--- lib/glitter/cli.rb
+++ lib/glitter/cli.rb
@@ -9,8 +9,10 @@ module Glitter
     method_option :notes,           :type => :string, :aliases => "-n"
     method_option :force,           :type => :boolean, :aliases => "-f"
     method_option :bundle_version,  :type => :string, :aliases => "-b"
+    method_option :minimum_system_version, :type => :string, :aliases => "-m"
     def push(executable_path, *asset_paths)
       release = Release::Sparkle.new(channel, options.version)
+      release.minimum_system_version = options.minimum_system_version
       release.bundle_version = options.bundle_version
       release.notes       = options.notes
       release.executable  = File.open executable_path
